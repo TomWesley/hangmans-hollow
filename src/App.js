12b6ba0b9d552ece7270915fea5f5984239b0f4a
Thomas Wesley <tomjwesley@gmail.com>
2022-02-05 13:33:20 -0500

    Change Messaging icon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react'
 import Letters from './Letters'
 import { useState } from 'react'
 import AnswerLetters from './AnswerLetters'
-//import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics'
+// import { initializeApp } from 'firebase/app'
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyC7KRHKPJUlp997AFgUN1FwwbWxOZf1mII',
+//   authDomain: 'singularity-c216f.firebaseapp.com',
+//   databaseURL: 'https://singularity-c216f.firebaseio.com',
+//   projectId: 'singularity-c216f',
+//   storageBucket: 'singularity-c216f.appspot.com',
+//   messagingSenderId: '877374644269',
+//   appId: '1:877374644269:web:7e8a5f141d2572661b98a4',
+//   measurementId: 'G-J997WH1WTT',
+// }
+// const app = initializeApp(firebaseConfig)
+
 let p = localStorage.getItem('puzzleLetters')
 if (p) {
   if (JSON.parse(p)[0].name != 'V') {
