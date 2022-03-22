cec8c8f7411c466df9128837bb44fb9d4290355e
Thomas Wesley <tomjwesley@gmail.com>
2022-03-21 23:31:46 -0400

    new puzzle

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,42 +1,37 @@
 export default [
   {
     id: 1,
-    name: 'F',
+    name: 'M',
     isHidden: true,
   },
   {
     id: 2,
-    name: 'L',
-    isHidden: true,
-  },
-  {
-    id: 3,
-    name: 'I',
-    isHidden: true,
-  },
-  {
-    id: 4,
-    name: 'P',
-    isHidden: true,
-  },
-  {
-    id: 5,
-    name: 'P',
-    isHidden: true,
-  },
-  {
-    id: 6,
     name: 'A',
     isHidden: true,
   },
   {
-    id: 7,
-    name: 'N',
+    id: 3,
+    name: 'L',
     isHidden: true,
   },
   {
-    id: 8,
-    name: 'T',
+    id: 4,
+    name: 'A',
+    isHidden: true,
+  },
+  {
+    id: 5,
+    name: 'I',
+    isHidden: true,
+  },
+  {
+    id: 6,
+    name: 'S',
+    isHidden: true,
+  },
+  {
+    id: 7,
+    name: 'E',
     isHidden: true,
   },
 ]
