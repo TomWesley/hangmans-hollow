cbd26f4678d4a48433c6a0959a8c47e4c8b58bc7
Thomas Wesley <tomjwesley@gmail.com>
2025-03-27 13:52:18 -0400

    Better bat animation

--- src/BatMeter.js
+++ src/BatMeter.js
@@ -1,24 +1,30 @@
 // src/BatMeter.js
 import React from 'react';
-import batOutline from './images/bat-outline.png'; // We'll add this to your src/images folder
+import batOutline from './bat-outline.png'; // Make sure this is in your src/images folder
 
 const BatMeter = ({ score, maxScore }) => {
   // Calculate fill percentage (inverted so it fills up as score decreases)
-  const fillPercentage = ((maxScore - score) / maxScore) * 100;
+  // At score = maxScore (full health), we want 0% filled
+  // At score = 0 (game over), we want 100% filled
+  const fillPercentage = 20+((maxScore - score) / maxScore) * 80;
   
   return (
     <div className="bat-meter-container">
       <div className="bat-wrapper">
-        <div className="bat-outline" style={{ backgroundImage: `url(${batOutline})` }}>
-          <div 
-            className="bat-fill" 
-            style={{ 
-              height: `${fillPercentage}%`,
-              WebkitMaskImage: `url(${batOutline})`,
-              maskImage: `url(${batOutline})`
-            }}
-          ></div>
-        </div>
+        {/* The visible bat image with clip-path applied */}
+        <div 
+          className="bat-image" 
+          style={{ 
+            backgroundImage: `url(${batOutline})`,
+            clipPath: `inset(${100-fillPercentage}% 0 0 0)` // Clip from top
+          }}
+        ></div>
+        
+        {/* The outline of the bat that's always visible */}
+        <div 
+          className="bat-outline"
+          style={{ backgroundImage: `url(${batOutline})` }}
+        ></div>
       </div>
       <div className="misses-text">
         Misses Remaining: {score}
